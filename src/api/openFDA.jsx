7d952732cb6e5175f28c0aa2edc1318473a50c0e
Garik Asatryan <asa7ur@gmail.com>
2024-06-19 23:00:32 +0200

    Experimentando diferentes formas de filtrar resultados

--- src/api/openFDA.jsx
+++ src/api/openFDA.jsx
@@ -3,16 +3,46 @@ import axios from 'axios'
 const BASE_URL = 'https://api.fda.gov/drug/label.json'
 
 export const searchDrugs = async (query) => {
-  try {
-    const response = await axios.get(BASE_URL, {
-      params: {
-        search: query,
-        limit: 10,
-      },
-    })
-    return response.data.results || []
-  } catch (error) {
-    console.error('Error fetching data from OpenFDA', error)
-    return []
+  let combinedResults = []
+  const searchLimit = 15 // Puedes ajustar el límite de resultados según sea necesario
+
+  for (let i = 1; i <= query.length; i++) {
+    const partialQuery = query.substring(0, i)
+    try {
+      const response = await axios.get(BASE_URL, {
+        params: {
+          search: partialQuery, // Ajuste del parámetro de búsqueda
+          limit: searchLimit,
+        },
+      })
+
+      if (response.data.results) {
+        combinedResults = combinedResults.concat(response.data.results)
+      }
+    } catch (error) {
+      // if (error.response && error.response.status === 404) {
+      //   console.warn(`No results found for query segment: ${partialQuery}`)
+      // } else {
+      //   console.error('Error fetching data from OpenFDA', error)
+      // }
+    }
   }
+
+  // Filtrar resultados para incluir solo los más relevantes
+  const filteredResults = combinedResults.filter(
+    (result) =>
+      result.openfda.brand_name &&
+      result.openfda.brand_name.some((name) =>
+        name.toLowerCase().includes(query.toLowerCase())
+      )
+  )
+
+  // Eliminar duplicados
+  const uniqueResults = Array.from(
+    new Set(filteredResults.map((result) => JSON.stringify(result)))
+  ).map((result) => JSON.parse(result))
+
+  const limitedResults = uniqueResults.slice(0, 5)
+
+  return limitedResults
 }
