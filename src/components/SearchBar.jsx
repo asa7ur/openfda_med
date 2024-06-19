25c99fa9ae8898cf256763d0effe542e572deffe
Garik Asatryan <asa7ur@gmail.com>
2024-06-19 23:25:46 +0200

    He añadido progreso circular al esperar a los resultados

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,20 +1,29 @@
 import { useState } from 'react'
-import { TextField, Button, Box } from '@mui/material'
+import {
+  TextField,
+  Button,
+  Box,
+  CircularProgress,
+  Typography,
+} from '@mui/material'
 import { searchDrugs } from '../api/openFDA'
 
 const SearchBar = ({ setResults, onSearch }) => {
   const [query, setQuery] = useState('')
+  const [loading, setLoading] = useState(false)
 
   const handleSearch = async () => {
+    setLoading(true)
     const results = await searchDrugs(query)
     setResults(results)
+    setLoading(false)
     if (onSearch) {
       onSearch()
     }
   }
 
   return (
-    <Box sx={{paddingTop: '2rem'}} >
+    <Box sx={{ paddingTop: '2rem' }}>
       <TextField
         label='Buscar medicamento'
         value={query}
@@ -26,6 +35,21 @@ const SearchBar = ({ setResults, onSearch }) => {
       <Button variant='contained' color='primary' onClick={handleSearch}>
         Buscar
       </Button>
+      {loading && (
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            marginTop: '1rem',
+          }}
+        >
+          <CircularProgress />
+          <Typography variant='body1' sx={{ marginTop: '1rem' }}>
+            Buscando...
+          </Typography>
+        </Box>
+      )}
     </Box>
   )
 }
