eeb77dd47662217bbff4f9b2d943dc585bf0dc14
Garik Asatryan <asa7ur@gmail.com>
2024-06-19 17:10:29 +0200

    Lista de resultado añadido

---
+++ src/components/ResultsList.jsx
@@ -0,0 +1,28 @@
+import { List, ListItem, ListItemText, Box, Paper } from '@mui/material'
+
+const ResultsList = ({ results }) => {
+  return (
+    <Box sx={{ padding: '0 10rem', background: 'transparent' }}>
+      <List >
+        {results.map((result, index) => {
+          const brandName = result.openfda?.brand_name?.[0]
+          const genericName = result.openfda?.generic_name?.[0]
+
+          if (!brandName || !genericName) {
+            return null
+          }
+
+          return (
+            <Paper sx={{ background: 'transparent' }} key={index}>
+              <ListItem button onClick={() => setSelectedResult(result)}>
+                <ListItemText primary={brandName} secondary={genericName} />
+              </ListItem>
+            </Paper>
+          )
+        })}
+      </List>
+    </Box>
+  )
+}
+
+export default ResultsList
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import SearchBar from '../components/SearchBar'
+import ResultsList from '../components/ResultsList'
 
 const Search = () => {
   const [results, setResults] = useState([])
   return (
     <>
       <SearchBar setResults={setResults} />
+      <ResultsList results={results} />
     </>
   )
 }
