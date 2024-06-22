8957863a7a19bd020e393c19c1b5cd842a6e5f2e
Garik Asatryan <asa7ur@gmail.com>
2024-06-22 12:51:44 +0200

    Añadido fade a la pagina de inicio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,21 @@
-import { Stack, Box, Typography } from '@mui/material'
+import React, { useState, useEffect } from 'react'
+import { Stack, Box, Typography, Fade } from '@mui/material'
 import image from '../assets/doctors.svg'
 import Search from '../components/Search'
 
 const Home = () => {
+  const [showText, setShowText] = useState(false)
+  const [showSearch, setShowSearch] = useState(false)
+
+  useEffect(() => {
+    setShowText(true)
+    const timer = setTimeout(() => {
+      setShowSearch(true)
+    }, 500)
+
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
     <>
       <Stack
@@ -12,14 +25,24 @@ const Home = () => {
         justifyContent='space-around'
       >
         <Box sx={{ width: '600px', position: 'relative', marginTop: '-5rem' }}>
-          <Typography variant='h1'>Buscador</Typography>
-          <Typography variant='h5'>
-            Busca información sobre medicamentos y sus efectos secundarios
-          </Typography>
-          <Search />
+          <Fade in={showText} timeout={1000}>
+            <div>
+              <Typography variant='h1'>Buscador</Typography>
+              <Typography variant='h5'>
+                Busca información sobre medicamentos y sus efectos secundarios
+              </Typography>
+            </div>
+          </Fade>
+          <Fade in={showSearch} timeout={1000}>
+            <div>
+              <Search />
+            </div>
+          </Fade>
         </Box>
         <Box>
-          <img src={image} alt='Doctors' width='400px' />
+          <Fade in={showText} timeout={1000}>
+            <img src={image} alt='Doctors' width='400px' />
+          </Fade>
         </Box>
       </Stack>
     </>
