b2910d52aa9d9fa00558871d214b2268fad35c11
Garik Asatryan <asa7ur@gmail.com>
2024-06-22 16:45:06 +0200

    Home más responsivo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,20 @@
 import React, { useState, useEffect } from 'react'
-import { Stack, Box, Typography, Fade } from '@mui/material'
+import {
+  Stack,
+  Box,
+  Typography,
+  Fade,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material'
 import image from '../assets/doctors.svg'
 import Search from '../components/Search'
 
 const Home = () => {
   const [showText, setShowText] = useState(false)
   const [showSearch, setShowSearch] = useState(false)
+  const theme = useTheme()
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'))
 
   useEffect(() => {
     setShowText(true)
@@ -19,16 +28,31 @@ const Home = () => {
   return (
     <>
       <Stack
-        padding='10rem 0'
-        direction='row'
+        padding={{ xs: '5rem 0', md: '10rem 0' }}
+        direction={{ xs: 'column', md: 'row' }}
         alignItems='center'
-        justifyContent='space-around'
+        justifyContent='center'
+        spacing={4}
       >
-        <Box sx={{ width: '600px', position: 'relative', marginTop: '-5rem' }}>
+        <Box
+          sx={{
+            width: { xs: '100%', md: '600px' },
+            position: 'relative',
+            textAlign: { xs: 'center', md: 'left' },
+          }}
+        >
           <Fade in={showText} timeout={1000}>
             <div>
-              <Typography variant='h1'>Buscador</Typography>
-              <Typography variant='h5'>
+              <Typography
+                variant='h1'
+                sx={{ fontSize: { xs: '2rem', md: '3rem' } }}
+              >
+                Buscador
+              </Typography>
+              <Typography
+                variant='h5'
+                sx={{ fontSize: { xs: '1rem', md: '1.5rem' } }}
+              >
                 Busca información sobre medicamentos y sus efectos secundarios
               </Typography>
             </div>
@@ -39,12 +63,21 @@ const Home = () => {
             </div>
           </Fade>
         </Box>
-        <Box>
+        {isSmallScreen && (
+          <Box>
+            <Fade in={showText} timeout={1000}>
+              <img src={image} alt='Doctors' width='300px' />
+            </Fade>
+          </Box>
+        )}
+      </Stack>
+      {!isSmallScreen && (
+        <Stack justifyContent='center' alignItems='center' marginTop='2rem'>
           <Fade in={showText} timeout={1000}>
             <img src={image} alt='Doctors' width='400px' />
           </Fade>
-        </Box>
-      </Stack>
+        </Stack>
+      )}
     </>
   )
 }
